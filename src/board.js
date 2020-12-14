e31cc56f7c4b00dc5f36507e1be1f41db9c73acf
Eliashib <59631669+EliJ91@users.noreply.github.com>
2020-12-14 13:39:27 -0500

    BackEnd Built

--- src/board.js
+++ src/board.js
@@ -5,43 +5,50 @@ export class OutOfRange extends Error{}
 
 export class Board {
   constructor() {
-    this._state = [
-      [1,2,3],
-      [4,5,6],
-      [7,8,9]
-    ]
+    this._state = [0,1,2,3,4,5,6,7,8]
+    //[0,1,2]
+    //[3,4,5]
+    //[6,7,8]
   }
 
   put(token,index){
     if (token != "X" && token != "Y") {
       throw new InvalidToken(`${token} is not a valid token, please use X or Y`)
     }
-    if (index < 1  || index > 9){
+    if (index < 0  || index > 8){
         throw new OutOfRange(`${index} is out of acceptable range, please select a number between 1 and 9`)
     }
-    const [row, col] = this._coordinatesFromIndex(index)
-    return this._state[row][col] = token
+    return this._state[index] = token
   }
 
   at(index){
-    if (index < 1  || index > 9){
+    if (index < 1  || index > 8){
         throw new OutOfRange(`${index} is out of acceptable range, please select a number between 1 and 9`)
     }
-    const [row, col] = this._coordinatesFromIndex(index)
-    return this._state[row][col]
+    return this._state[index]
   }
 
-  rows(){
+  status(){
       return this._state
   }
 
-  _coordinatesFromIndex(index) {
-    const size = 3
-    const zeroBase = index - 1
+  columnWin(token){
+    if([this._state[0],this._state[3],this._state[6]].every((i)=> i == token )) return [token,[0,3,6]]
+    if([this._state[1],this._state[4],this._state[7]].every((i)=> i == token )) return [token,[1,4,7]]
+    if([this._state[2],this._state[5],this._state[8]].every((i)=> i == token )) return [token,[2,5,8]]
+    return false
+  }
 
-    const row = Math.floor(zeroBase / size)
-    const col = zeroBase % size
+  rowWin(token){
+    if([this._state[0],this._state[1],this._state[2]].every((i)=> i == token )) return [token,[0,1,2]]
+    if([this._state[3],this._state[4],this._state[5]].every((i)=> i == token )) return [token,[3,4,5]]
+    if([this._state[6],this._state[7],this._state[8]].every((i)=> i == token )) return [token,[6,7,8]]
+    return false
+  }
 
-    return [row, col]
+  diagonalWin(token){
+    if([this._state[0],this._state[4],this._state[8]].every((i)=> i == token )) return [token,[0,4,8]]
+    if([this._state[2],this._state[4],this._state[6]].every((i)=> i == token )) return [token,[2,4,6]]
+    return false
   }
 }
--- src/board.test.js
+++ src/board.test.js
@@ -3,56 +3,123 @@ import {describe, test, expect} from '@jest/globals'
 import { Board } from './board'
 
 describe("Board", () => {
+
   describe("setting and getting squares", ()=>{
-    test("can set specific square value", () => {
-      const board = new Board()
-      board.put("X", 1)
-      expect(board.at(1)).toBe("X")
+      test("can set specific square value", () => {
+        const board = new Board()
+        board.put("X", 1)
+        expect(board.at(1)).toBe("X")
+      })
+
+      test("passing non X or Y throws an invalid character error when setting", ()=>{
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
+        const board = new Board()
+        expect(()=> board.put("Z", 1)).toThrowError()
+      })
+
+      test("passing position below 1 or above 9 to put results in out of range error", ()=>{
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
+        const board = new Board()
+        expect(()=> board.put("Y", 10)).toThrowError()
+      })
+
+      test("passing position below 1 or above 9 to at results in out of range error", ()=>{
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
+        const board = new Board()
+        expect(()=> board.at(10)).toThrowError()
+      })
+      test("board updates are in correct squares", ()=>{
+        const board = new Board()
+        board.put("X", 0)
+        board.put("Y", 4)
+        const status = board.status()
+        expect(status).toEqual(["X", 1, 2, 3,"Y", 5, 6, 7, 8])
+      })
     })
 
-    test("passing non X or Y throws an invalid character error when setting", ()=>{
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
-      const board = new Board()
-      expect(()=> board.put("Z", 1)).toThrowError()
+  describe("Board.status", ()=>{
+      test("returns the rows of the board", ()=>{
+        const board = new Board()
+        const rows = board.status()
+        expect(rows).not.toBe(null)
+      })
     })
 
-    test("passing position below 1 or above 9 to put results in out of range error", ()=>{
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
-      const board = new Board()
-      expect(()=> board.put("Y", 10)).toThrowError()
+  describe("Board.checkColumnWins", ()=>{
+      test("returns if column 1 contains a win", ()=>{
+        const board = new Board()
+        for (const number of [0,3,6]) {
+          board.put("X", number);
+        }
+        const win = board.columnWin("X")
+        expect(win).toEqual(["X",[0,3,6]])
+      })
+
+      test("returns if column 2 contains a win", ()=>{
+        const board = new Board()
+        for (const number of [1,4,7]) {
+          board.put("Y", number);
+        }
+        const win = board.columnWin("Y")
+        expect(win).toEqual(["Y",[1,4,7]])
+      })
+
+      test("returns if column 3 contains a win", ()=>{
+        const board = new Board()
+        for (const number of [2,5,8]) {
+          board.put("X", number);
+        }
+        const win = board.columnWin("X")
+        expect(win).toEqual(["X",[2,5,8]])
+      })
     })
 
-    test("passing position below 1 or above 9 to at results in out of range error", ()=>{
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
+  describe("Board.checkRowWins", ()=>{
+    test("returns if row 1 contains a win", ()=>{
       const board = new Board()
-      expect(()=> board.at(10)).toThrowError()
+      for (const number of [0,1,2]) {
+        board.put("X", number);
+      }
+      const win = board.rowWin("X")
+      expect(win).toEqual(["X",[0,1,2]])
+    })
+
+    test("returns if row 2 contains a win", ()=>{
+      const board = new Board()
+      for (const number of [3,4,5]) {
+        board.put("Y", number);
+      }
+      const win = board.rowWin("Y")
+      expect(win).toEqual(["Y",[3,4,5]])
+    })
+
+    test("returns if row 3 contains a win", ()=>{
+      const board = new Board()
+      for (const number of [6,7,8]) {
+        board.put("X", number);
+      }
+      const win = board.rowWin("X")
+      expect(win).toEqual(["X",[6,7,8]])
     })
   })
 
-  describe("Board.rows", ()=>{
-    test("returns the rows of the board", ()=>{
+  describe("Board.checkDiagonalWins", ()=>{
+    test("returns if diagonal 1 contains a win", ()=>{
       const board = new Board()
-      const rows = board.rows()
-      expect(rows).not.toBe(null)
+      for (const number of [0,4,8]) {
+        board.put("Y", number);
+      }
+      const win = board.diagonalWin("Y")
+      expect(win).toEqual(["Y",[0,4,8]])
     })
-    test("board updates are in correct squares", ()=>{
+    test("returns if diagonal 2 contains a win", ()=>{
       const board = new Board()
-      board.put("X", 1)
-      board.put("Y", 5)
-      const rows = board.rows()
-      expect(rows).toEqual([["X", 2, 3], [4, "Y", 6], [7, 8, 9]])
+      for (const number of [2,4,6]) {
+        board.put("X", number);
+      }
+      const win = board.diagonalWin("X")
+      expect(win).toEqual(["X",[2,4,6]])
     })
   })
 
-  describe("Board.columns", ()=>{
-    test.skip("returns the columns of the board", ()=>{})
-    test.skip("columns has length of 3", ()=>{})
-    test.skip("each column has length of 3", ()=>{})
-  })
-
-  describe("Board.diagonals", ()=>{
-    test.skip("returns the diagonals of the board", ()=>{})
-    test.skip("diagonals has length of 2", ()=>{})
-    test.skip("each diagonal has length of 3", ()=>{})
-  })
 })
